4f5f414942af91bcc0885262b9566cd9be62e359
kong0527 <o____ri@naver.com>
2020-06-13 17:48:40 +0900

    petsitter 다중값 저장하기 완료. code.java 클래스 생성 & petsitter 도메인 수정

--- src/main/java/com/ssd/petMate/Controller/PetsitterController.java
+++ src/main/java/com/ssd/petMate/Controller/PetsitterController.java
@@ -1,5 +1,6 @@
 package com.ssd.petMate.Controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ssd.petMate.domain.Code;
 import com.ssd.petMate.domain.Info;
 import com.ssd.petMate.domain.Petsitter;
 import com.ssd.petMate.page.BoardSearch;
@@ -34,10 +36,46 @@ public class PetsitterController {
 		else return new Petsitter();
 	}
 	
+	@ModelAttribute("sizeCodes")
+	public List<Code> referenceData1() {
+		List<Code> sizeCodes = new ArrayList<Code>();
+		sizeCodes.add(new Code("1", "소형　　　"));
+		sizeCodes.add(new Code("2", "중형　　　"));
+		sizeCodes.add(new Code("4", "대형"));
+		
+		 return sizeCodes;
+	}
+	
+	@ModelAttribute("dayCodes")
+	public List<Code> referencData2() {
+		List<Code> dayCodes = new ArrayList<Code>();
+		dayCodes.add(new Code("1", "월요일　　"));
+		dayCodes.add(new Code("2", "화요일　　"));
+		dayCodes.add(new Code("4", "수요일　　"));
+		dayCodes.add(new Code("8", "목요일　　"));
+		dayCodes.add(new Code("16", "금요일　　"));
+		dayCodes.add(new Code("32", "토요일　　"));
+		dayCodes.add(new Code("64", "일요일"));
+		
+		return dayCodes;
+	}
+	
 	@PostMapping("/petsitterInsert")
 	public String petsitterInsert(@ModelAttribute("petsitter") Petsitter petsitter, SessionStatus sessionStatus, HttpServletRequest request) {
 		sessionStatus.setComplete();
+		int sizeSum = 0;
+		int daySum = 0;
 		petsitter.setUserID("test4");
+		for (String s : petsitter.getSizeCodes()) {
+			sizeSum += Integer.parseInt(s);
+		}
+		petsitter.setPetSize(Integer.toString(sizeSum));
+		
+		for (String s : petsitter.getDayCodes()) {
+			daySum += Integer.parseInt(s);
+		}
+		petsitter.setPetDay(Integer.toString(daySum));
+		
 		petsitterFacade.insertBoard(petsitter);
 		return "redirect:/petsitterList";
 	}
@@ -68,9 +106,38 @@ public class PetsitterController {
 		@RequestMapping(value = "/petsitterDetail", method = { RequestMethod.GET, RequestMethod.POST })
 		public ModelAndView petsitterDetail(ModelAndView mv, @RequestParam("boardNum") int boardNum) {
 			Petsitter view = petsitterFacade.boardDetail(boardNum);
+			String size = "";
+			String day = "";
 			System.out.println(view);
+			
+			int sizeNum = Integer.parseInt(view.getPetSize());
+			if ((sizeNum & 1) != 0)
+				size += "소형 ";
+			if ((sizeNum & 2) != 0)
+				size += "중형 ";
+			if ((sizeNum & 4) != 0)
+				size += "대형 ";
+			
+			int dayNum = Integer.parseInt(view.getPetDay());
+			if ((dayNum & 1) != 0)
+				day += "월요일 ";
+			if ((dayNum & 2) != 0)
+				day += "화요일 ";
+			if ((dayNum & 4) != 0)
+				day += "수요일 ";
+			if ((dayNum & 8) != 0)
+				day += "목요일 ";
+			if ((dayNum & 16) != 0)
+				day += "금요일 ";
+			if ((dayNum & 32) != 0)
+				day += "토요일 ";
+			if ((dayNum & 64) != 0)
+				day += "일요일 ";
+
 			mv.addObject("petsitter", petsitterFacade.boardDetail(boardNum));
 			mv.setViewName("petsitter/petsitterDetail");
+			mv.addObject("size", size);
+			mv.addObject("day", day);
 			return mv;
 		}
 		
