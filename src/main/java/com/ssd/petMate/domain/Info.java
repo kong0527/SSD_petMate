25a8a5b7f35498ada6d175eb3d743a82709bb807
kong0527 <o____ri@naver.com>
2020-06-29 01:39:57 +0900

    일부 수정

--- src/main/java/com/ssd/petMate/domain/Info.java
+++ src/main/java/com/ssd/petMate/domain/Info.java
@@ -6,13 +6,10 @@ public class Info {
 	private int boardNum;
 	private String boardDate;
 	private String userID;
-//	임시로 해놓은 annotation
-	@NotEmpty
 	private String boardContent;
 	private int boardLike;
 	private int boardHit;
 	private int replyCnt;
-	@NotEmpty 
 	private String boardTitle;
 	
 	public int getBoardNum() {
