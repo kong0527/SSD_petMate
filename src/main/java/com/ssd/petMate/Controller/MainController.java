bc6dd20a7dc4140834fd7b9233fd015a9c912e04
nahyeonpark <64285899+nahyeonpark@users.noreply.github.com>
2020-06-29 14:41:16 +0900

    공구, 중고 장바구니 개선

--- src/main/java/com/ssd/petMate/Controller/MainController.java
+++ src/main/java/com/ssd/petMate/Controller/MainController.java
@@ -23,46 +23,41 @@ public class MainController {
 	@Autowired
 	private BestFacade bestFacade;
 	
-//	@RequestMapping(value = "/index", method = { RequestMethod.GET, RequestMethod.POST })
-//	public ModelAndView index(ModelAndView mv) {
-//		mv.setViewName("index");
-//		return mv;
-//	}
-	
 	@RequestMapping(value = "/index", method = { RequestMethod.GET, RequestMethod.POST })
-	public ModelAndView main(ModelAndView mv, HttpServletRequest request) {
-		
-		Date mon = new Date();
-        try {
-           Calendar cal = Calendar.getInstance();
-           cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-           mon = cal.getTime();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        List<Info> weeklyBestInfo = bestFacade.weeklyBestInfo(mon);	
-        Info dailyBestInfo = bestFacade.dailyBestInfo();
-        
-        List<Inquiry> weeklyBestInquiry = bestFacade.weeklyBestInquiry(mon);
-        Inquiry dailyBestInquiry = bestFacade.dailyBestInquiry();
-        
-        List<Review> weeklyBestReview = bestFacade.weeklyBestReview(mon);
-        Review dailyBestReview = bestFacade.dailyBestReview();
-                
-		mv.addObject("weeklyBestInfo", weeklyBestInfo);
-		mv.addObject("weeklyBestInquiry", weeklyBestInquiry);
-		mv.addObject("weeklyBestReview", weeklyBestReview);
-		mv.addObject("dailyBestInfo", dailyBestInfo);
-		mv.addObject("dailyBestInquiry", dailyBestInquiry);
-		mv.addObject("dailyBestReview", dailyBestReview);
+	public ModelAndView index(ModelAndView mv) {
 		mv.setViewName("index");
 		return mv;
 	}
 	
-//	@RequestMapping(value = "/paymentForm", method = { RequestMethod.GET, RequestMethod.POST })
-//	public ModelAndView paymentForm(ModelAndView mv) {
-//		mv.setViewName("paymentForm");
+//	@RequestMapping(value = "/index", method = { RequestMethod.GET, RequestMethod.POST })
+//	public ModelAndView main(ModelAndView mv, HttpServletRequest request) {
+//		
+//		Date mon = new Date();
+//        try {
+//           Calendar cal = Calendar.getInstance();
+//           cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+//           mon = cal.getTime();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        List<Info> weeklyBestInfo = bestFacade.weeklyBestInfo(mon);	
+//        Info dailyBestInfo = bestFacade.dailyBestInfo();
+//        
+//        List<Inquiry> weeklyBestInquiry = bestFacade.weeklyBestInquiry(mon);
+//        Inquiry dailyBestInquiry = bestFacade.dailyBestInquiry();
+//        
+//        List<Review> weeklyBestReview = bestFacade.weeklyBestReview(mon);
+//        Review dailyBestReview = bestFacade.dailyBestReview();
+//                
+//		mv.addObject("weeklyBestInfo", weeklyBestInfo);
+//		mv.addObject("weeklyBestInquiry", weeklyBestInquiry);
+//		mv.addObject("weeklyBestReview", weeklyBestReview);
+//		mv.addObject("dailyBestInfo", dailyBestInfo);
+//		mv.addObject("dailyBestInquiry", dailyBestInquiry);
+//		mv.addObject("dailyBestReview", dailyBestReview);
+//		mv.setViewName("index");
 //		return mv;
-//	}	
+//	}
+
 }
--- src/main/java/com/ssd/petMate/Controller/MyPageController.java
+++ src/main/java/com/ssd/petMate/Controller/MyPageController.java
@@ -325,4 +325,38 @@ public class MyPageController {
 		mv.setViewName("mypage/myInquiryLikePage");
 		return mv;
 	}
+	
+	@RequestMapping(value = "/myReviewLike", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView mypageReviewLike(ModelAndView mv, HttpServletRequest request,
+			@RequestParam(required = false, defaultValue = "1") int pageNum,
+			@RequestParam(required = false, defaultValue = "10") int contentNum,
+			@RequestParam(required = false) String searchType,
+			@RequestParam(required = false) String keyword) {
+		
+		String userID = request.getSession().getAttribute("userID").toString();
+		System.out.println(userID);
+		
+//		검색한 결과값을 가져오기 위해 map에 키워드와 검색 타입 저장 후 sql 쿼리에 삽입
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("keyword", keyword);
+		map.put("searchType", searchType);
+		map.put("userID", userID);
+
+		int totalCount = myPageFacade.getPrivateInquiryLikeCount(map);
+		System.out.println(totalCount);
+		
+		BoardSearch boardSearch = new BoardSearch();
+		boardSearch.setSearchType(searchType);
+		boardSearch.setKeyword(keyword);
+		boardSearch.setUserID(userID);
+	
+//		페이징과 검색 기능이 적용된 후의 리스트를 가지고 옴
+		boardSearch.pageInfo(pageNum, contentNum, totalCount);
+		List<Inquiry> myInquiryList = myPageFacade.getPrivateInquiryLike(boardSearch);
+
+		mv.addObject("myInquiryList", myInquiryList);
+		mv.addObject("boardSearch", boardSearch);
+		mv.setViewName("mypage/myInquiryLikePage");
+		return mv;
+	}
 }
